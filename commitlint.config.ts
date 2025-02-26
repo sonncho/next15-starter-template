d846b8ac3c93212e13b8ba120fcacadcadd4a489
sonchowon <dungcho93@gmail.com>
2025-02-26 20:27:21 +0900

    docs(docs): add commitlint prompt config

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -52,6 +52,108 @@ const Configuration: UserConfig = {
       ],
     ],
   },
+  prompt: {
+    settings: {},
+    messages: {
+      skip: ':skip',
+      max: 'maximum %d chars',
+      min: 'minimum %d chars',
+      emptyWarning: 'can not be empty',
+      upperLimitWarning: 'over limit',
+      lowerLimitWarning: 'below limit',
+    },
+    questions: {
+      type: {
+        description: "Select the type of change you're committing:",
+        enum: {
+          feat: {
+            description: 'A new feature',
+            title: 'Features',
+            emoji: '✨',
+          },
+          fix: {
+            description: 'A bug fix',
+            title: 'Bug Fixes',
+            emoji: '🐛',
+          },
+          docs: {
+            description: 'Documentation only changes',
+            title: 'Documentation',
+            emoji: '📚',
+          },
+          style: {
+            description: 'Changes that do not affect the meaning of the code (formatting, etc)',
+            title: 'Styles',
+            emoji: '💎',
+          },
+          refactor: {
+            description: 'A code change that neither fixes a bug nor adds a feature',
+            title: 'Code Refactoring',
+            emoji: '📦',
+          },
+          perf: {
+            description: 'A code change that improves performance',
+            title: 'Performance Improvements',
+            emoji: '🚀',
+          },
+          test: {
+            description: 'Adding missing tests or correcting existing tests',
+            title: 'Tests',
+            emoji: '🚨',
+          },
+          build: {
+            description: 'Changes that affect the build system or external dependencies',
+            title: 'Builds',
+            emoji: '🛠',
+          },
+          ci: {
+            description: 'Changes to CI configuration files and scripts',
+            title: 'Continuous Integrations',
+            emoji: '⚙️',
+          },
+          chore: {
+            description: "Other changes that don't modify src or test files",
+            title: 'Chores',
+            emoji: '♻️',
+          },
+          revert: {
+            description: 'Reverts a previous commit',
+            title: 'Reverts',
+            emoji: '🗑',
+          },
+        },
+      },
+      scope: {
+        description: 'What is the scope of this change (e.g. component or file name):',
+      },
+      subject: {
+        description: 'Write a short, imperative tense description of the change:',
+      },
+      body: {
+        description: 'Provide a longer description of the change (optional):',
+      },
+      isBreaking: {
+        description: 'Are there any breaking changes?',
+      },
+      breakingBody: {
+        description:
+          'A BREAKING CHANGE commit requires a body. Please describe the breaking change:',
+      },
+      breaking: {
+        description: 'Describe the breaking changes:',
+      },
+      isIssueAffected: {
+        description: 'Does this change affect any open issues?',
+      },
+      issuesBody: {
+        description:
+          'If issues are closed, the commit requires a body. Please describe the issues:',
+      },
+      issues: {
+        description: 'Add issue references (e.g. "fix #123", "re #123".):',
+      },
+    },
+  },
 };
 
 export default Configuration;
